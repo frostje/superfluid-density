911928fe643ae053c543b8093967e810313b299f
frostje <frostje@uni.edu>
2013-06-05 12:11:26 -0500

    Wed June 5

--- 1D_Code.f95
+++ 1D_Code.f95
@@ -2,40 +2,43 @@ program tight_binding_model
 
 	!SCALAR VARIABLES
 	integer, parameter :: N=10, LWMAX = 1000, output=10
-	real, parameter :: t=-1.0, KB = 8.613E-5
+	double precision, parameter :: t=-1.0, KB = 8.613E-5
 	character, parameter :: JOBZ='V', UPLO='U'
+	complex :: i = -1
 	integer :: LDA=N, INFO, LWORK, temp
-	real B, Nl, Ne, RoTar, Ro, MuGs, high, low
+	real :: B, Nl, Ne, RoTar, Ro, MuGs, high, low
 	
 	
 	!ARRAY VARIABLES
-	double precision   RWORK(3*N-2), W(N)
+	double precision   RWORK(3*N-2), E(N)
 	complex*16         H0(N,N), WORK(LWMAX)
 
 	!EXTERNAL SUBROUTINES
 	external ZHEEV
 
 	!INTRINSIC FUNCTIONS
-	intrinsic int, min
+	intrinsic int, min, sqrt
+
+	i = sqrt(i)
 
 	!CONSTRUCTING THE HAMILTONIAN
-	do i=1, n
-		do j=1,n
-			if (abs(j-i) == 1) then
-				H0(i,j) = t
-			elseif (abs(j-i) == (N-1)) then
-				H0(i,j) = t
+	do ix=1, N
+		do jx=1, N
+			if (abs(jx-ix) == 1) then
+				H0(ix,jx) = t
+			elseif (abs(jx-ix) == (N-1)) then
+				H0(ix,jx) = t
 			else
-				H0(i,j) = 0
+				H0(ix,jx) = 0
 			endif
 		enddo
 	enddo
 
 	LWORK = -1
-	call ZHEEV(JOBZ, UPLO, N, H0, LDA, W, WORK, LWORK, RWORK, INFO)
+	call ZHEEV(JOBZ, UPLO, N, H0, LDA, E, WORK, LWORK, RWORK, INFO)
 	LWORK = min( LWMAX, int(WORK(1)))
 
-	call ZHEEV(JOBZ, UPLO, N, H0, LDA, W, WORK, LWORK, RWORK, INFO)
+	call ZHEEV(JOBZ, UPLO, N, H0, LDA, E, WORK, LWORK, RWORK, INFO)
 	
 	if (INFO == 0) then
 		write(*,*)'SUCCESS'
@@ -44,8 +47,8 @@ program tight_binding_model
 	!WRITING EIGENVALUES TO FILE
 	open(unit=output,file='results.txt',action='write',status='replace')
 	write(output,*) 'The eigenvalues are:'
-	do i=1,n
-		write(output,*) W(i)
+	do l=1, n
+		write(output,*) E(l)
 	enddo
 
 	!GETTING TEMP AND DENSITY FROM USER
@@ -55,13 +58,13 @@ program tight_binding_model
 	read(*,*) RoTar
 
 	!CALCULATING FIRST MuGs AND Ro
-	high = W(N)
-	low = W(1)	
+	high = E(N)
+	low = E(1)	
 	MuGs = (high+low) / 2
 	B = 1/(KB*temp)
 	Ne = 0
-	do i=1,N
-		Nl = 1/(exp(B*(W(i)-MuGs)) + 1)
+	do l=1,N
+		Nl = 1/(exp(B*(E(l)-MuGs)) + 1)
 		Ne = Ne + (2*Nl)
 	enddo
 	
@@ -82,8 +85,8 @@ program tight_binding_model
 		
 		!MODIFYING Ro		
 		Ne = 0
-		do i=1,N
-			Nl = 1/(exp(B*(W(i)-MuGs)) + 1)
+		do l=1,N
+			Nl = 1/(exp(B*(E(l)-MuGs)) + 1)
 			Ne = Ne + (2*Nl)
 		enddo
 	
